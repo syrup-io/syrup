d1c3e01603f1f8b0e67ab4731098b0df4120c00a
jacob zlogar <jacob.zlogar@gmail.com>
2016-09-13 19:00:52 -0400

    check to see if repos and orgs are arrays, maybe theres a better way to do this

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,9 +2,17 @@
 
 @section('content')
 
-@if (Auth::check() and isset($orgs))
+@if (Auth::check() and is_array($orgs) and is_array($repos))
 
   @include('project.orgs')
+
+  @include('project.repos')
+
+@elseif (Auth::check() and is_array($orgs))
+
+  @include('project.orgs')
+
+@elseif (Auth::check() and is_array($repos))
 
   @include('project.repos')
 
