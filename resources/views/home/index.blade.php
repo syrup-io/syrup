e71a1359f412be01dcdffda2e60951b80b0a8be7
jacob zlogar <jacob.zlogar@gmail.com>
2016-09-13 19:11:19 -0400

    is_array

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,19 +2,15 @@
 
 @section('content')
 
-@if (Auth::check() and is_array($orgs) and is_array($repos))
+@if (Auth::check())
 
-  @include('project.orgs')
+  @if(is_array($orgs))
+    @include('project.orgs')
+  @endif
 
-  @include('project.repos')
-
-@elseif (Auth::check() and is_array($orgs))
-
-  @include('project.orgs')
-
-@elseif (Auth::check() and is_array($repos))
-
-  @include('project.repos')
+  @if(is_array($repos))
+    @include('project.repos')
+  @endif
 
 @endif
 
